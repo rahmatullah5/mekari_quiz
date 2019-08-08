7a6facebf19285d3f9e6b67f7af7ca19bd3bc499
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 20:04:14 +0700

    refactor route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
-  resources :quizzes
-  resources :levels
+  # resources :quizzes
+  resources :levels, only: [:index, :show] do
+    resources :quizzes, only: [:index, :show] do
+      post :answer
+    end
+  end
+
+  root 'levels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
