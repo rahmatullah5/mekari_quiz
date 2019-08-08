5d1d27e9716fcbf471254d6b17548d8b5899ca5d
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 15:05:51 +0700

    annotate level

--- app/models/level.rb
+++ app/models/level.rb
@@ -5,9 +5,14 @@
 #  id          :bigint           not null, primary key
 #  cover       :string
 #  description :text
+#  slug        :string
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_levels_on_slug  (slug) UNIQUE
 #
 
 class Level < ApplicationRecord
