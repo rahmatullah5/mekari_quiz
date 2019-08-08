ad9019b4147fd68a24bd2b7c16610c5293fa7f48
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 14:10:48 +0700

    annotate quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: quizzes
+#
+#  id          :bigint           not null, primary key
+#  answer      :string
+#  cover       :string
+#  hint        :text
+#  hint_images :string
+#  point       :integer          default(0)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  level_id    :bigint
+#
+# Indexes
+#
+#  index_quizzes_on_level_id  (level_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (level_id => levels.id)
+#
+
 class Quiz < ApplicationRecord
   belongs_to :level
 end
