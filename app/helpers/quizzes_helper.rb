2c5485813d060386a69183f1aa67428c95dd50f4
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 20:24:10 +0700

    fix helper issue

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,17 @@
 module QuizzesHelper
+  def answer_alert(status)
+    if status.eql?('success')
+      "
+      <div class='alert alert-success' role='alert'>
+      Wooo, That's the correct answer.
+      </div>
+      "
+    else
+      "
+      <div class='alert alert-danger' role='alert'>
+      Sorry, you should try another answer.
+      </div>
+      "
+    end
+  end
 end
