4013fed3e9d1e17066000b1540625a6d66c20d95
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 20:08:38 +0700

    refactor quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,74 +1,37 @@
 class QuizzesController < ApplicationController
-  before_action :set_quiz, only: [:show, :edit, :update, :destroy]
-
+ skip_before_action :verify_authenticity_token, only: [:answer]
+  before_action :set_quiz, only: [:answer]
+  before_action :set_level
   # GET /quizzes
   # GET /quizzes.json
   def index
-    @quizzes = Quiz.all
+    @quiz = @level.quizzes.order('RANDOM()').first
   end
 
-  # GET /quizzes/1
-  # GET /quizzes/1.json
-  def show
-  end
 
-  # GET /quizzes/new
-  def new
-    @quiz = Quiz.new
-  end
-
-  # GET /quizzes/1/edit
-  def edit
-  end
-
-  # POST /quizzes
-  # POST /quizzes.json
-  def create
-    @quiz = Quiz.new(quiz_params)
-
-    respond_to do |format|
-      if @quiz.save
-        format.html { redirect_to @quiz, notice: 'Quiz was successfully created.' }
-        format.json { render :show, status: :created, location: @quiz }
-      else
-        format.html { render :new }
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
+  def answer
+    if @quiz.correct_answer?(quiz_params[:answer].downcase)
+      session[:point] += @quiz.point
+      flash[:msg] = 'success'
+    else
+      flash[:msg] = 'failed'
     end
+
+    redirect_to level_quizzes_path(@level)
   end
 
-  # PATCH/PUT /quizzes/1
-  # PATCH/PUT /quizzes/1.json
-  def update
-    respond_to do |format|
-      if @quiz.update(quiz_params)
-        format.html { redirect_to @quiz, notice: 'Quiz was successfully updated.' }
-        format.json { render :show, status: :ok, location: @quiz }
-      else
-        format.html { render :edit }
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quizzes/1
-  # DELETE /quizzes/1.json
-  def destroy
-    @quiz.destroy
-    respond_to do |format|
-      format.html { redirect_to quizzes_url, notice: 'Quiz was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_quiz
-      @quiz = Quiz.find(params[:id])
+      @quiz = Quiz.find(params[:quiz_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_level
+      @level = Level.find(params[:level_id])
+    end
+
     def quiz_params
-      params.require(:quiz).permit(:level_id, :hint_images, :hint, :cover, :answer, :point)
+      params.require(:quiz).permit(:answer)
     end
 end
