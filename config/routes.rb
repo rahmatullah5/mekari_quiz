77fab4f5dea60433be87e0077fa417ac181ad4ad
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2019-08-08 23:22:15 +0700

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :admins
-  # resources :quizzes
   resources :levels, only: [:index, :show] do
-    resources :quizzes, only: [:index, :show] do
+    resources :quizzes, only: [:index] do
       post :answer
     end
   end
 
   root 'levels#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
